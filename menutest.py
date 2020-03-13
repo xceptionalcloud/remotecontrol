9f4050f0c10681cae49b0be332493eef916efa10
xndev <alangford@xceptional.com>
2020-03-12 18:34:31 -0700

    Menu

--- menutest.py
+++ menutest.py
@@ -26,13 +26,13 @@ print("Starting PhoneRemote test")
 #_88xx_keynavlist = ['Key:KeyPad1', 'Key:KeyPad2', 'Key:KeyPad0', 'Key:KeyPad3', 'Key:KeyPadPound']
 #_88xx_keynavlist = ['Key:KeyPad9', 'Key:KeyPad3', 'Key:KeyPad4', 'Key:KeyPad1', 'Key:KeyPadPound']
 #_88xx_keynavlist = ['Key:Soft2']
-_88xx_keynavlist = ['Play:sunshine.raw']
+#_88xx_keynavlist = ['Play:sunshine.raw']
 #_88xx_keynavlist = ['Key:Applications','Key:KeyPad6']
 #_88xx_keynavlist = ['Key:Applications','Key:KeyPad6','Key:KeyPad4','Key:KeyPad1','Key:Soft3']
 #_88xx_keynavlist = ['Key:Applications','Key:KeyPadStar','Key:KeyPadStar','Key:KeyPadPound','Key:KeyPadStar','Key:KeyPadStar']
 #_88xx_keynavlist = ['Key:Applications','Key:KeyPad5','Key:KeyPad4','Key:KeyPad4','Key:Soft3']
 #_88xx_keynavlist = ['Key:Applications'] #,'Key:KeyPad6','Key:KeyPad4','Key:KeyPad1','Key:Soft3']
-#_88xx_keynavlist = ['Key:KeyPad2']
+_88xx_keynavlist = ['This is a menu']
 
 alphaCCap = ['Key:KeyPad2', 'Key:KeyPad2', 'Key:KeyPad2', 'Key:KeyPad2', 'Key:KeyPad2', 'Key:KeyPad2', 'Key:KeyPad2']
 alphaI = ['Key:KeyPad4', 'Key:KeyPad4', 'Key:KeyPad4']
@@ -47,10 +47,11 @@ url = 'http://10.255.20.93/CGI/Execute'
 headers={'content-type':'application/xml'}
 
 for keypress in _88xx_keynavlist:
-    ph_nav = etree.Element('CiscoIPPhoneExecute')
-    exeit_e = etree.SubElement(ph_nav, 'ExecuteItem')
-    exeit_e.set('Priority','0')
-    exeit_e.set('URL',keypress)
+    ph_nav = etree.Element('CiscoIPPhoneMenu')
+    exeit_e = etree.SubElement(ph_nav, 'Title')
+    #exeit_e.set('Priority','0')
+    #exeit_e.set('URL',keypress)
+    exeit_e.text = keypress
     phnavstr = etree.tostring(ph_nav,pretty_print=True)
     keynav[key] = keynav[key]+[phnavstr]
 '''
